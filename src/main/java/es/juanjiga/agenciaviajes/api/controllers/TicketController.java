d44bbcde7be5bd6f2344c82c70976d389b64417c
juanjiga <juanjiga71@hotmail.es>
2024-04-27 13:26:33 +0200

    casasabado

--- src/main/java/es/juanjiga/agenciaviajes/api/controllers/TicketController.java
+++ src/main/java/es/juanjiga/agenciaviajes/api/controllers/TicketController.java
@@ -1,5 +1,7 @@
 package es.juanjiga.agenciaviajes.api.controllers;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,11 @@ import es.juanjiga.agenciaviajes.api.models.response.TicketResponse;
 import es.juanjiga.agenciaviajes.infrastructure.abstract_services.ITicketService;
 import lombok.AllArgsConstructor;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping(path = "ticket")
@@ -20,12 +25,19 @@ public class TicketController {
 
     private final ITicketService ticketService;
 
-    @CrossOrigin("http://127.0.0.1:5500")
+    //@CrossOrigin("http://127.0.0.1:5500")
     @PostMapping
     public ResponseEntity<TicketResponse> post(@RequestBody TicketRequest request) {
 
         return ResponseEntity.ok(ticketService.create(request));
 
     }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<TicketResponse> get(@PathVariable UUID id) {
+        return ResponseEntity.ok(ticketService.read(id));
+    }
+    
+    
 
 }
